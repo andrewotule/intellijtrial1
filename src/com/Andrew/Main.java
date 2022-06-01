59d30ea7045ca4a76428f11be12f7ccd31badf3f
Andrew Otule <andrewotule2@gmail.com>
2022-06-01 13:18:01 +0300

    Added a for loop

--- src/com/Andrew/Main.java
+++ src/com/Andrew/Main.java
@@ -1,11 +1,20 @@
 package com.Andrew;
 
+import java.sql.SQLOutput;
+
 public class Main {
-    //this is a test program
+    //this is a trial program
     public static void main(String[] args) {
 	int x = 15;
     int y = 10;
     int sum = x + y;
-    System.out.print("the sum of " + x + " and " + y + " is " + sum);
+    System.out.println("the sum of " + x + " and " + y + " is " + sum);
+
+
+    // Testing a for loop
+    String[] animals = {"Goat", "Cow" , "Donkey", "Dog"};
+        for(int i = 0; i < animals.length; i++ ){
+            System.out.println("Animal " + (i + 1) + " is a " + animals[i]);
+        }
     }
 }
