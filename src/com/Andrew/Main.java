8452657a851c1ab43eceaba047b9d069b59868a4
EPIXX <the.benefactoer@gmail.com>
2022-06-01 12:44:42 +0300

    Initial commit

---
+++ src/com/Andrew/Main.java
@@ -0,0 +1,11 @@
+package com.Andrew;
+
+public class Main {
+    //this is a test program
+    public static void main(String[] args) {
+	int x = 15;
+    int y = 10;
+    int sum = x + y;
+    System.out.print("the sum of " + x + " and " + y + " is " + sum);
+    }
+}
